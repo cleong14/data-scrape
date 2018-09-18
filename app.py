1e885af2f3a938cd1f07bda07fd692b703f388f7
Chaz Leong <cleong14@gmail.com>
2018-09-18 01:39:11 -1000

    Able to scrape data and write to csv. TODO: complex data scraping

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
-import urllib2
+from urllib.request import urlopen
 #import requests as req #windows py3
 #import urllib.request #windows
 from bs4 import BeautifulSoup
 
 #1 getting web page:
-web_page = 'https://finance.yahoo.com/quote/FB?p=FB'
+web_page = 'https://www.hawaiianhumane.org/dog-adoptions/'
 
 #query the website and return the html to the variable page:
-page = urllib2.urlopen(web_page)
+page = urlopen(web_page)
 #page = req.get(web_page)
 #urllib.request.Request(web_page) #windows
 #print(page)
 
 soup = BeautifulSoup(page, 'html.parser')
-#soup = BeautifulSoup(page.text, 'html.parser')
+# soup = BeautifulSoup(page.text, 'html.parser')
+# print(soup)
 
-name_box = soup.find('h1', attrs={'class': 'D(ib)'})
-# print(name_box)
-name = name_box.text
-print(name)
+title_box = soup.find('h1', attrs={'class': 'entry-title'})
+print('AGE BELOW')
+print(title_box)
+title = title_box.text
+print(title)
 
-price_box = soup.find('span', attrs={'class': 'Fw(b)'})
-price = price_box.text
-print(price)
+# price_box = soup.find('span', attrs={'class': 'Fw(b)'})
+# price = price_box.text
+# print(price)
 
 import csv
 from datetime import datetime
 
 with open('index.csv', 'a') as csv_file: 
      writer = csv.writer(csv_file) 
-     writer.writerow([name, price, datetime.now()]) 
+     writer.writerow([title, datetime.now()]) 
 
 
 
